8ccd0110fa265402a84fee32051aac5ec44470bf
Lukas Reithmeier <lukas.reithmeier@gmail.com>
2022-04-18 21:58:07 +0200

    added tests for integration methods

---
+++ tests/simulate/test_integration_methods.py
@@ -0,0 +1,68 @@
+"""
+Integration methods tests
+"""
+import math
+
+import pytest
+
+from simulate.integrators import integration_methods
+
+
+@pytest.mark.parametrize(
+    "current, step_size, expected",
+    [
+        [1, 0.01, 1.01],
+        [0, 0.01, 0.0],
+        [1, 0.1, 1.1],
+    ],
+)
+def test_euler(current, step_size, expected):
+    """valid inputs"""
+    # given
+    # when
+    result = integration_methods.euler(
+        current=current, diff_func=lambda v: v, step_size=step_size
+    )
+
+    # then
+    assert math.isclose(result, expected)
+
+
+@pytest.mark.parametrize(
+    "current, step_size, expected",
+    [
+        [1, 0.01, 1.01005],
+        [0, 0.01, 0.0],
+        [1, 0.1, 1.105],
+    ],
+)
+def test_heun(current, step_size, expected):
+    """valid inputs"""
+    # given
+    # when
+    result = integration_methods.heun(
+        current=current, diff_func=lambda v: v, step_size=step_size
+    )
+
+    # then
+    assert math.isclose(result, expected)
+
+
+@pytest.mark.parametrize(
+    "current, step_size, expected",
+    [
+        [1, 0.01, 1.0100501670833333],
+        [0, 0.01, 0.0],
+        [1, 0.1, 1.1051708333333334],
+    ],
+)
+def test_runge_kutta(current, step_size, expected):
+    """valid inputs"""
+    # given
+    # when
+    result = integration_methods.runge_kutta(
+        current=current, diff_func=lambda v: v, step_size=step_size
+    )
+
+    # then
+    assert math.isclose(result, expected)
